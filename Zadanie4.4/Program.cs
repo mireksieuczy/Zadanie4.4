1d08f02d487545cf503146733b0b5d6001ae7df5
Mirek <Mirek@DESKTOP-ROAP5PV>
2022-06-22 18:26:41 +0200

    Dodaj pliki projektów.

---
+++ Zadanie4.4/Program.cs
@@ -0,0 +1,40 @@
+﻿Random rand = new Random();
+
+int i;
+int j;
+int k;
+int[] tab;
+tab = new int[100];
+
+for (i = 0; i < 100; i++)
+{
+    tab[i] = rand.Next(1, 1000);
+    Console.Write("{0,8}", tab[i]);
+}
+
+Console.WriteLine("Liczby pierwsze w powyzszym zbiorze: ");
+
+int suma = 0;
+for (j = 0; j < tab.Length; j++)
+{
+    for (k = 2; k < tab[j]; k++)
+
+        if (tab[j] % k == 0)
+        {
+
+            tab[j] = 0;
+            break;
+        }
+    if (k == tab[j])
+        Console.Write("{0,8}", tab[j]);
+    {
+        if (tab[j] > 0)
+            tab[j] = 0 + 1;
+        suma = suma + tab[j];
+
+    }
+}
+Console.WriteLine("\n     W podanym zakresie znajduje sie {0} liczb pierwszych.", suma);
+
+
+Console.ReadKey();
